fdfe3d5dd33b5449190c2122f4ff2b9b218741e1
Alonske <pizzaslice987@gmail.com>
2022-10-27 23:30:00 +1100

    building headType Label from graphic path

--- Source/PawnLayerOptionHead.cs
+++ Source/PawnLayerOptionHead.cs
@@ -7,9 +7,23 @@ using Verse;
 
 namespace EdB.PrepareCarefully {
     public class PawnLayerOptionHead : PawnLayerOption {
+        protected (string, int) splitCamel(string input) {
+            StringBuilder builder = new StringBuilder();
+            int seperatorCount = 0;
+            foreach(char c in input) {
+                if (Char.IsUpper(c) && builder.Length > 0) {
+                    seperatorCount += 1;
+                    builder.Append(' ');
+                };
+                builder.Append(c);
+            }
+            return (builder.ToString(), seperatorCount);
+        }
         public override string Label {
             get {
-                return HeadType.label;
+                string[] splitGraphicPath = HeadType.graphicPath.Split('_');
+                (string, int) jawPart1 = splitCamel(splitGraphicPath[1]);
+                return jawPart1.Item2 > 0 ? jawPart1.Item1 : splitCamel(splitGraphicPath[1]).Item1 + " " + splitGraphicPath[2];
             }
             set {
                 throw new NotImplementedException();
